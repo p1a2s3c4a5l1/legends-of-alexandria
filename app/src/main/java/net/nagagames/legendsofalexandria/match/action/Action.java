6dc15dd68ad991f66020d65947dce464ba0b941b
RaphaelMillion <raphael.m024@gmail.com>
2017-12-04 20:58:26 +0100

    Added structural classes and first card-interface (character)

---
+++ app/src/main/java/net/nagagames/legendsofalexandria/match/action/Action.java
@@ -0,0 +1,15 @@
+package net.nagagames.legendsofalexandria.match.action;
+
+/**
+ * Actions include effects like buffs, attribute modifiers, direct damage, creating entities, etc
+ * This class should be as general as possible to include all possibilities
+ *
+ * @author Raphael Million
+ */
+
+public class Action {
+
+    static Action concatenate(Action a1, Action a2) {
+        return null;
+    }
+}
