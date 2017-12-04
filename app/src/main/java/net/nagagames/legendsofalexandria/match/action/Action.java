2a5c5373c314b35a8fdd0072952efbbc42b48c4b
RaphaelMillion <raphael.m024@gmail.com>
2017-12-04 21:29:14 +0100

    Added documentation

--- app/src/main/java/net/nagagames/legendsofalexandria/match/action/Action.java
+++ app/src/main/java/net/nagagames/legendsofalexandria/match/action/Action.java
@@ -9,7 +9,16 @@ package net.nagagames.legendsofalexandria.match.action;
 
 public class Action {
 
+
+    /**
+     * In order to combine two actions into one, this action is required.
+     * This is useful when dealing with a single action
+     *
+     * @param a1    The first action to combine
+     * @param a2    The second action to combine
+     * @return the combined actions of a1 and a2
+     */
     static Action concatenate(Action a1, Action a2) {
-        return null;
+        return null; //TODO concatenate should bind two action objects
     }
 }
