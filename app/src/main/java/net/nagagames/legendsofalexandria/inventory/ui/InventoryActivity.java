56393911cead3a8e51ab8a048f82fcc84b1900db
p1a2s3c4a5l1 <34208674+p1a2s3c4a5l1@users.noreply.github.com>
2018-02-14 17:41:24 +0100

    test2

---
+++ app/src/main/java/net/nagagames/legendsofalexandria/inventory/ui/InventoryActivity.java
@@ -0,0 +1,22 @@
+package net.nagagames.legendsofalexandria.inventory.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import net.nagagames.legendsofalexandria.R;
+
+/**
+ *
+ *
+ * @author Pascal
+ */
+public class InventoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+
+
+}
--- app/src/main/java/net/nagagames/legendsofalexandria/menu/ui/MainMenuActivity.java
+++ app/src/main/java/net/nagagames/legendsofalexandria/menu/ui/MainMenuActivity.java
@@ -1,8 +1,9 @@
 package net.nagagames.legendsofalexandria.menu.ui;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import net.nagagames.legendsofalexandria.R;
 
@@ -18,5 +19,11 @@ public class MainMenuActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+
+    public void toast(View v) {
+        Toast t = Toast.makeText(this,"Du magst Bananen", Toast.LENGTH_LONG);
+        t.show();
     }
 }
